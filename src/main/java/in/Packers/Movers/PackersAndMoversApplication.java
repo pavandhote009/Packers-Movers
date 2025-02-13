c668815917f09f6e7240aec2b6f92f74d0c1c452
pavandhote009 <pavandhote009@gmail.com>
2025-02-13 11:19:04 +0530

    initial commit

---
+++ src/main/java/in/Packers/Movers/PackersAndMoversApplication.java
@@ -0,0 +1,13 @@
+package in.Packers.Movers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackersAndMoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackersAndMoversApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/Packers/Movers/PackersAndMoversApplicationTests.java
@@ -0,0 +1,13 @@
+package in.Packers.Movers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackersAndMoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
